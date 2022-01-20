4d995941adfd790377f0a077bbcc4d72cd30c55e
Narnillian <94580999+Narnillian@users.noreply.github.com>
2022-01-19 23:44:28 -0800

    Update wordle-function.py

--- cassidoo-challenges/#231---Wordle/wordle-function.py
+++ cassidoo-challenges/#231---Wordle/wordle-function.py
@@ -1,14 +1,17 @@
-#Written by Narnillian, starting on January 19, 2021. Mostly copied from the code of the full game I made, which is also in this Github repo
+#Written by Narnillian, also entirely on January 19, 2021. It wasn't that hard, because it was mostly copied from the code of the full game I made, which is also in this Github repo
 #Written in response to cassidoo's "Interview question of the week" from Newsletter #231, Jan. 16 2022
 
-#Make sure to run with `python3 -i` so you can use the function
-def wordleGuess(guess, solution):
+#Make sure to run `python3 -i`
+
+from random import choice
+
+def wordleGuess(guess, solution=None):
+    if not solution:
+        solution = solution_word
     if len(guess) != 5:
         print("Please make sure your guess has exactly 5 letters.")
-    
     guessed_word = guess.lower()
     copy_of_solution = solution
-
     for letter in range(5):
         if guessed_word[letter] == copy_of_solution[letter]:
             print("🟩", end="")
@@ -22,3 +25,6 @@ def wordleGuess(guess, solution):
             print("⬜", end="")
             continue
     print()
+
+solutions = ["fudge", "party", "parks", "guess", "solve", "print", "debug", "rules", "great", "thing", "write", "reply", "these", "stuff", "looks", "jocks", "jumbo", "fuzzy", "pizza", "baker", "cabin", "earth", "words", "might", "every", "match", "green", "sound", "could", "trier", "peppy", "apple", "power", "robot"]
+solution_word = choice(solutions)
