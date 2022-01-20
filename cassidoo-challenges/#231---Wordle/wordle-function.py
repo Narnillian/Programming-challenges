220604df740aad905d9c59addbf05a24bb5bf4d6
Narnillian <94580999+Narnillian@users.noreply.github.com>
2022-01-20 10:49:37 -0800

    Update wordle-function.py
    Finishing touches from full game ported here

--- cassidoo-challenges/#231---Wordle/wordle-function.py
+++ cassidoo-challenges/#231---Wordle/wordle-function.py
@@ -1,4 +1,6 @@
-#Written by Narnillian, also entirely on January 19, 2021. It wasn't that hard, because it was mostly copied from the code of the full game I made, which is also in this Github repo
+#Written by Narnillian, almost entirely on January 19, 2021.
+#Mostly copied from the code of the full game I made, which is also in this Github repo
+#When I updated the game on Jan. 20, I also updated here
 #Written in response to cassidoo's "Interview question of the week" from Newsletter #231, Jan. 16 2022
 
 #Make sure to run `python3 -i`
@@ -19,12 +21,14 @@ def wordleGuess(guess, solution=None):
             continue
         elif guessed_word[letter] in copy_of_solution:
             print("🟨", end="")
-            copy_of_solution = copy_of_solution.replace(guessed_word[letter], ' ') #i thank you my friend, Onmert, for this line
+            copy_of_solution = copy_of_solution.replace(guessed_word[letter], ' ', 1) #i thank you my friend, Onmert, for this line
             continue
         else:
             print("⬜", end="")
             continue
     print()
 
-solutions = ["fudge", "party", "parks", "guess", "solve", "print", "debug", "rules", "great", "thing", "write", "reply", "these", "stuff", "looks", "jocks", "jumbo", "fuzzy", "pizza", "baker", "cabin", "earth", "words", "might", "every", "match", "green", "sound", "could", "trier", "peppy", "apple", "power", "robot"]
+solutions = ["fudge", "party", "parks", "guess", "solve", "print", "debug", "rules", "great", "thing", "write", "reply",
+                "these", "stuff", "looks", "jocks", "jumbo", "fuzzy", "pizza", "baker", "cabin", "earth", "words", "might",
+                "every", "match", "green", "sound", "could", "trier", "peppy", "apple", "power", "robot", "again", "space"]
 solution_word = choice(solutions)
