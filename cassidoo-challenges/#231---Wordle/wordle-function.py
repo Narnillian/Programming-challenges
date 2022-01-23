e6c7288c69064309b2abfa7ae9ad2c6bcc86dce3
Narnillian <94580999+Narnillian@users.noreply.github.com>
2022-01-22 22:11:29 -0800

    Update wordle-function.py

--- cassidoo-challenges/#231---Wordle/wordle-function.py
+++ cassidoo-challenges/#231---Wordle/wordle-function.py
@@ -1,6 +1,6 @@
 #Written by Narnillian, almost entirely on January 19, 2021.
-#Mostly copied from the code of the full game I made, which is also in this Github repo
-#When I updated the game on Jan. 20, I also updated here
+#Mostly copied from the code of the full game I made, which is in the same directory of this Github repo
+#When I updated the game on Jan. 20 and 22, I also updated here
 #Written in response to cassidoo's "Interview question of the week" from Newsletter #231, Jan. 16 2022
 
 #Make sure to run `python3 -i`
@@ -10,9 +10,12 @@ from random import choice
 def wordleGuess(guess, solution=None):
     if not solution:
         solution = solution_word
-    if len(guess) != 5:
-        print("Please make sure your guess has exactly 5 letters.")
     guessed_word = guess.lower()
+    if len(guessed_word) != 5:
+        print("Please make sure your guess has exactly 5 letters."); return
+    for letter in guessed_word:
+        if letter not in alphabet:
+            print("Please make sure all the letters in your guess are in the 26 letter English alphabet")
     copy_of_solution = solution
     for letter in range(5):
         if guessed_word[letter] == copy_of_solution[letter]:
@@ -28,7 +31,9 @@ def wordleGuess(guess, solution=None):
             continue
     print()
 
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 solutions = ["fudge", "party", "parks", "guess", "solve", "print", "debug", "rules", "great", "thing", "write", "reply",
                 "these", "stuff", "looks", "jocks", "jumbo", "fuzzy", "pizza", "baker", "cabin", "earth", "words", "might",
-                "every", "match", "green", "sound", "could", "trier", "peppy", "apple", "power", "robot", "again", "space"]
+                "every", "match", "green", "sound", "could", "trier", "peppy", "apple", "power", "robot", "again", "space",
+                "pixel", "phone", "bowls", "range"]
 solution_word = choice(solutions)
