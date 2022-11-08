1dab1d4280ae4f46e02dbcc6701132cba78b6759
Narnillian <ner.baby@gmail.com>
2022-11-08 15:16:00 -0800

    Anti-Divisors!

---
+++ cassidoo-challenges/#273---Anti-div/README.md
@@ -0,0 +1,18 @@
+# Anti-Divisors
+
+Find every anti-divisor of a given integer. This link was provided for more information: https://oeis.org/A066272/a066272a.html
+
+
+## About
+
+Could have been completed in one day, if I didn't start it at 10PM right before bedtime. I barely had to do any work for this challenge, because the link above tells how to figure out if `k` is an anti-div of `n`.
+
+<br>
+
+## Usage
+
+`python antidiv.py [number]` - Prints out anti-divisors of `number`.
+
+`python -i antidiv.py` - Opens interactive Python prompt, from which you can run `antidivisor([number])` as was specified in the Newsletter.
+
+Both options utilize the same function, which means that they print out the same result.
---
+++ cassidoo-challenges/#273---Anti-div/antidiv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# reading https://oeis.org/A066272/a066272a.html begun 2022-11-06 (sunday)
+# code begun 2022-11-07
+
+from sys import flags as callflags
+from sys import argv
+
+def isAD(k,n):
+    return n%k==k/2 if k%2==0 else n%k==(k-1)/2 or n%k==(k+1)/2
+
+def antidivisor(n):
+    print("[",end="")
+    for k in range(2,n):
+        if isAD(k,n):
+            print(k,end="")
+            print(",",end="")
+    print("\b]") #hacky solution to get rid of extra comma because i can't math
+
+
+# i learned something new!
+if callflags.inspect == 0:
+    if len(argv)>1: antidivisor(int(argv[1]))
+    else: print(
+        """Please include a number as another argument, or run this script in inspect mode.
+For example:
+    python antidiv.py 15
+    python -i antidiv.py""")
